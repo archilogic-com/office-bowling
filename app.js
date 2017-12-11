6ea30040c5c2313104053a4c4d6c97659ab8145f
Martin Splitt <mr.avgp@googlemail.com>
2017-12-11 17:33:32 +0100

    Adds the game files

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const scene = document.querySelector('a-scene')
 const scoreboard = document.getElementById('scoreboard')
-const SCORE_TEXT_LEN = 3
+const SCORE_TEXT_LEN = 2
+const INACTIVITY_TIMEOUT = 30
 const ball = document.getElementById('ball')
-let score = 0
+//const timer = document.getElementById('timer')
+
+let score = 0, timeLeft = INACTIVITY_TIMEOUT
 
 scene.addEventListener('renderstart', function () {
   const cam = document.querySelector('[camera]')
 
-  window.addEventListener('touchpadup', () => {
+  window.addEventListener('triggerup', () => {
+    timeLeft = INACTIVITY_TIMEOUT
+  })
+
+  window.addEventListener('gripup', () => {
+    timeLeft = INACTIVITY_TIMEOUT
     ball.removeAttribute('dynamic-body')
     const pos = cam.getAttribute('position')
-    ball.setAttribute('position', `${pos.x} 0.5 ${pos.z}`)
+    ball.setAttribute('position', `${pos.x} 1 ${pos.z}`)
     ball.setAttribute('dynamic-body', 'mass: 1000; shape: sphere; sphereRadius: 0.2')
   })
 
@@ -24,6 +32,11 @@ scene.addEventListener('renderstart', function () {
       prepareFurnitureForFun(elem)
       document.getElementById('holder').appendChild(elem)
     })
+  })
+
+  scene.addEventListener('model-loaded', function startTimer() {
+    setInterval(tickTock, 1000)
+    scene.removeEventListener('model-loaded', startTimer)
   })
 })
 
@@ -48,4 +61,12 @@ function onCollide(evt) {
   let scoreString = score.toString(), len = scoreString.length
   for (let i = len; i < SCORE_TEXT_LEN; i++) scoreString = '0' + scoreString
   scoreboard.setAttribute('text-geometry', { value: scoreString })
+}
+
+function tickTock() {
+  if (--timeLeft < 0) {
+//    localStorage.setItem('currentScore', score)
+    window.location.href = '/'
+  }
+//  timer.setAttribute('text-geometry', { value: `${('00' + Math.floor(timeLeft / 60)).slice(-2)}:${('00' + (timeLeft % 60)).slice(-2)}` })
 }
